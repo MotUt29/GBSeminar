fb6f27af57ef9e0ab2d3addc944941ef037388f8
unknown <v-iru-s29rus@mail.ru>
2023-02-26 23:19:15 +0300

    первая задача семинара

---
+++ Seminar001_01/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его в квадрат (число умноженное само на себя)
+Console.Write("Введите целое число: ");
+int namber = int.Parse(Console.ReadLine());
+int resault = namber * namber;
+Console.WriteLine(resault);
