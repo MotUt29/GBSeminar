0e96fbb8e0c8a0eecbdc38205e587e7e9fa29cf1
unknown <v-iru-s29rus@mail.ru>
2023-03-06 22:22:56 +0300

    3 задача 3 семинара

---
+++ Seminar03_03/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает произведение чисел от 1 до N
+
+// Возвращение произвведнеия чисел от 1 до N
+int GetMultipliction(int num)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        result *= 1;
+    }
+    return result;
+}
+
+Console.Write("Ввудеите число: ");
+int num = int.Parse(Console.ReadLine);
+
+int countNum = GetMultipliction(num);
+Console.WriteLine(countNum);
