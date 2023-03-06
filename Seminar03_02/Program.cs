b63570c3885c222ce780e673f354a11dc8fbc5a4
unknown <v-iru-s29rus@mail.ru>
2023-03-06 21:30:05 +0300

    2 задача 3 семинара

---
+++ Seminar03_02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдает колличество цифр в числе.
+//Возвращает количество цифр в числе
+int GetCountNum(int num)
+{
+    int count = 0;
+    int sourseNum = num;
+    while(sourseNum > 0)
+    {
+        sourseNum = sourseNum / 10;
+        count++;
+    }
+
+    return count;
+}
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int countNum = GetCountNum(num);
+
+Console.WriteLine(countNum);
