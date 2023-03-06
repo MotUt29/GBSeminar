99417e8e7d3c12e7c8818c9e577ab7ca03dff011
unknown <v-iru-s29rus@mail.ru>
2023-03-06 19:14:07 +0300

    Задача №10

---
+++ HW02_01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int Prompt(string message)
+{
+    Console.Write(message);
+    String value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+int number = Prompt("Введите трехзначное число > ");
+if(number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввкели не трехзгачное чило, пожалуйста повторите ввод: ");
+    return;
+}
+Console.WriteLine($"Введенное число `{number}`");
+int SecondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра `{SecondRank}");
