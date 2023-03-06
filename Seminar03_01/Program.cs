5a62ff2867461115a524d37e23350fd4a4132091
unknown <v-iru-s29rus@mail.ru>
2023-03-06 20:37:17 +0300

    1 задача 3 семинара

---
+++ Seminar03_01/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 134431
+bool isPalindrom(int number)
+{
+    int revNumber = 0;
+    int soursNumber = number;
+
+    while (soursNumber != 0)
+    {
+        int pop = soursNumber % 10;
+        soursNumber = soursNumber / 10;
+        revNumber = (revNumber * 10) + pop;
+    }
+    bool isResult = revNumber == number;
+    return isResult;
+}
