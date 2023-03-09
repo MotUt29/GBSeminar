0e7201c1e4b07eefc5c613f1f3f17e52777b224c
unknown <v-iru-s29rus@mail.ru>
2023-03-09 19:24:56 +0300

    1 домашняя задача 3 семинара

--- HW03_01/Program.cs
+++ HW03_01/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 // 134431
 bool isPalindrom(int number)
 {
