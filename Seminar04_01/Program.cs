56fb45daa9706078ebd5b1c08e898675073c8e8d
unknown <v-iru-s29rus@mail.ru>
2023-03-16 19:37:25 +0300

    4 семинар

---
+++ Seminar04_01/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+//Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+int GetSumPositiveElements(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+        
+        if(array[i] > 0 )
+        {
+            sum += array[i];
+        }
+    return sum;
+}
+
+int GetSumNegativeElements(int[ array])
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+        
+        if(array[i] < 0 )
+        {
+            sum += array[i];
+        }
+    return sum;
+}
+void PrintArrayToConsole(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        PrintArrayToConsole.Write(array[i] + "   ");
+    }
+    PrintArrayToConsole.WriteLine();
+}
+
+//---------------------------------------
